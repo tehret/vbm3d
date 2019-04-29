801661e94cc652e34264d4c6f1950b6c47c083e6
Thibaud EHRET <thibaud.ehret@ens-cachan.fr>
2019-04-29 10:49:07 +0200

    Fixing typo

--- src/utils/VBM3Ddenoising_multiscale.sh
+++ src/utils/VBM3Ddenoising_multiscale.sh
@@ -71,7 +71,7 @@ done
 for ((lvl=LEVELS-1; lvl>=0; --lvl))
 do
     sigma=$(bc <<< "scale=2; $NOISE / ${R_PYR}^$lvl")
-    ./VBM3Ddenoising -i levels/level_${lvl}_%04d.tiff -deno denoised/level_${lvl}_%04d.tiff -f ${FIRST} -l ${LAST} -add false --sigma $sigma $DEN_ARGS
+    ./VBM3Ddenoising -i levels/level_${lvl}_%04d.tiff -deno denoised/level_${lvl}_%04d.tiff -f ${FIRST} -l ${LAST} -add false -sigma $sigma $DEN_ARGS
 done
 
 wait
