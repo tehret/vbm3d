98b3120bf9233d8fef533a380e62a7275bd71cf5
Thibaud EHRET <thibaud.ehret@ens-cachan.fr>
2019-04-30 14:04:30 +0200

    Adding a forward and backward script for the optical flow

--- tvl1flow/tvl1flow_backward.sh
+++ tvl1flow/tvl1flow_backward.sh
@@ -8,8 +8,8 @@ O=${4:-""}
 
 for i in `seq $F $(($L - 1))`;
 do
-	./tvl1flow `printf $I $i` \
-        `printf $I $((i+1))` \
-		   `printf $O $i` \
-		   4 0.25 0.2 0.3 100 2 0.5 5 0.01 0; 
+    ./tvl1flow `printf $D $((i + 1))` \
+        `printf $D $i` \
+        `printf $O $((i + 1))` \
+        4 0.25 0.2 0.3 100 2 0.5 5 0.01 0;
 done
