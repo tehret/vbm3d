99514f677ec04051245170cb4057179a163b5ed1
Thibaud EHRET <thibaud.ehret@ens-cachan.fr>
2019-04-30 14:15:24 +0200

    Fixing mistake

--- tvl1flow/tvl1flow_backward.sh
+++ tvl1flow/tvl1flow_backward.sh
@@ -8,8 +8,8 @@ O=${4:-""}
 
 for i in `seq $F $(($L - 1))`;
 do
-    ./tvl1flow `printf $D $((i + 1))` \
-        `printf $D $i` \
+    ./tvl1flow `printf $I $((i + 1))` \
+        `printf $I $i` \
         `printf $O $((i + 1))` \
         4 0.25 0.2 0.3 100 2 0.5 5 0.01 0;
 done
