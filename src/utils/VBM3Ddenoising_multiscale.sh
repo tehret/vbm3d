b131fc990d3fd5655de69b7b97ea311a5916eb7a
parias <pariasm@gmail.com>
2020-03-18 09:23:22 +0100

    Improve ms script

--- src/utils/VBM3Ddenoising_multiscale.sh
+++ src/utils/VBM3Ddenoising_multiscale.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PATH_MULTISCALE=./
 
 if [ $# -lt 4 ]; then
     echo "$0 input_%03d.png sigma outputFolder out_%03d.tif"
@@ -8,7 +7,6 @@ if [ $# -lt 4 ]; then
     echo "    1     # index of the first frame of the video, (optional) default is 1"
     echo "    100   # index of the last frame of the video, (optional) default is 100"
     echo "    0     # type of multiscaler (0: DCT, 1: Gaussian, 2: Lanczos), default is 0 (optional)"
-    echo "    2     # R_PYR pyramid ratio: 2 (optional), 1.5 also possible"
     echo "    0.7   # PAR_PYR recomposition ratio : 0.7 (optional)"
     exit 1
 fi
@@ -23,58 +21,57 @@ FIRST=1
 LAST=100
 MSTYPE=0
 PAR_PYR=0.7
+R_PYR=2
 
+if [ -n "${6}" ];  then LEVELS=${6}  ; fi
+if [ -n "${7}" ];  then FIRST=${7}   ; fi
+if [ -n "${8}" ];  then LAST=${8}    ; fi
+if [ -n "${9}" ];  then MSTYPE=${9}  ; fi
+if [ -n "${10}" ]; then PAR_PYR=${10}; fi
 
-if [ -n "$6" ]; then
-    LEVELS=$6
-fi
-if [ -n "$7" ]; then
-    FIRST=$7
-fi
-if [ -n "$8" ]; then
-    LAST=$8
-fi
-if [ -n "$9" ]; then
-    MSTYPE=$9
-fi
-if [ -n "${10}" ]; then
-    PAR_PYR=${10}
-fi
+# we assume that the binaries are in the same folder as the script
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 MS_ARGS="-c ${PAR_PYR}"
 
-# Clean noisy
-rm -r noisy
-rm -r levels
-rm -r denoised
-mkdir noisy
-mkdir levels
-mkdir denoised
+# create folders
+mkdir -p $OUT/noisy
+mkdir -p $OUT/levels
+mkdir -p $OUT/denoised
 
-$PATH_MULTISCALE/addnoise -i ${INPUT} -sigma $NOISE -f ${FIRST} -l ${LAST}
+# add noise
+$DIR/addnoise -i ${INPUT} -sigma $NOISE -f ${FIRST} -l ${LAST} \
+	-o "$OUT/noisy/noisy_%04d.tiff"
 
 for ((frame=FIRST; frame<=LAST; ++frame))
 do
-    $PATH_MULTISCALE/decompose $(printf "noisy/noisy_%04d.tiff" $frame) levels/level_ ${LEVELS} _$(printf "%04d" $frame).tiff -t $MSTYPE
+    $DIR/decompose $(printf "$OUT/noisy/noisy_%04d.tiff" $frame) \
+		 "$OUT/levels/level_" ${LEVELS} "_"$(printf "%04d" $frame)".tiff" -t $MSTYPE
 done
 
 for ((lvl=LEVELS-1; lvl>=0; --lvl))
 do
     sigma=$(bc <<< "scale=2; $NOISE / ${R_PYR}^$lvl")
-    ./VBM3Ddenoising -i levels/level_${lvl}_%04d.tiff -deno denoised/level_${lvl}_%04d.tiff -f ${FIRST} -l ${LAST} -add false -sigma $sigma $DEN_ARGS
+    $DIR/VBM3Ddenoising -i "$OUT/levels/level_${lvl}_%04d.tiff" \
+		 -deno "$OUT/denoised/level_${lvl}_%04d.tiff" \
+		 -f ${FIRST} -l ${LAST} -add false -sigma $sigma $DEN_ARGS
 done
 
-wait
 for ((frame=FIRST; frame<=LAST; ++frame))
 do
-    $PATH_MULTISCALE/recompose denoised/level_ ${LEVELS} _$(printf "%04d" $frame).tiff $(printf $OUT/$OUTPUT $frame) -t $MSTYPE ${MS_ARGS}
+    $DIR/recompose $OUT/denoised/level_ ${LEVELS} \
+		 _$(printf "%04d" $frame).tiff $(printf $OUT/$OUTPUT $frame) \
+		 -t $MSTYPE ${MS_ARGS}
 done
 
-./psnr -i $OUT/$OUTPUT -r ${INPUT} -f ${FIRST} -l ${LAST} > $OUT/psnr.txt
+./psnr -i $OUT/denoised/level_0_%04d.tiff -r ${INPUT} -f ${FIRST} -l ${LAST} > $OUT/psnr-ss
+./psnr -i $OUT/$OUTPUT -r ${INPUT} -f ${FIRST} -l ${LAST} > $OUT/psnr-ms
 
-# Copy noisy to ouput folder
-mv noisy $OUT
-# Copy noisy to ouput folder
-mv levels $OUT
-# Copy first scale to output folder
-mv denoised $OUT
+# convert to png and remove tiffs
+ 
+## # Copy noisy to ouput folder
+## mv noisy $OUT
+## # Copy noisy to ouput folder
+## mv levels $OUT
+## # Copy first scale to output folder
+## mv denoised $OUT
